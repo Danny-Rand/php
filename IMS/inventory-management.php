f038008e2e5d24306e99cf32e5f476fb4c79b03f
Dan Miguela <danyvesmiguela@gmail.com>
2024-04-19 01:03:38 +0800

    12th Commit (Addition of user deletion feature)

--- IMS/inventory-management.php
+++ IMS/inventory-management.php
@@ -13,7 +13,7 @@ $products = include('database/products.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard - Igan's Budbod House IMS</title>
+    <title>Dashboard - Café Esque IMS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="CSS/dashboard.css">
--- IMS/user-add.php
+++ IMS/user-add.php
@@ -11,7 +11,7 @@ $users = include('database/show-users.php');
 <html>
 
 <head>
-    <title>Dashboard - Igan's Budbod House IMS</title>
+    <title>Dashboard - Café Esque IMS</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
@@ -81,6 +81,7 @@ $users = include('database/show-users.php');
                                             <th>Last Name</th>
                                             <th>Email</th>
                                             <th>Role</th>
+                                            <th>Action</th>
                                         <tr>
                                     </thead>
                                     <tbody>
@@ -92,7 +93,10 @@ $users = include('database/show-users.php');
                                                 <td><?= $user['last_name'] ?></td>
                                                 <td><?= $user['email'] ?></td>
                                                 <td><?= $user['role'] ?></td>
-
+                                                <td>
+                                                    <a href=""><i class="fa-solid fa-pencil"></i> Edit</a>
+                                                    <a href="" class="deleteUser" data-userid="<?= $user['ID'] ?>" data-fname="<?= $user['first_name'] ?>" data-lname="<?= $user['last_name'] ?>"><i class="fa-solid fa-trash-can"></i> Delete</a>
+                                                </td>
                                             </tr>
                                         <?php } ?>
                                     </tbody>
@@ -106,9 +110,59 @@ $users = include('database/show-users.php');
     </div>
 
     <script src="javascript/dashboard-script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="javascript/jquery/jquery-3.7.1.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        function script() {
+
+            this.initialize = function() {
+                this.registerEvents();
+            },
+
+            this.registerEvents = function() {
+                document.addEventListener('click', function(e){
+                    targetElement = e.target;
+                    classList = targetElement.classList;
+
+                    if(classList.contains('deleteUser')) {
+                        e.preventDefault();
+                        userId = targetElement.dataset.userid;
+                        fname = targetElement.dataset.fname;
+                        lname = targetElement.dataset.lname;
+                        fullName = fname + ' ' + lname;
+
+                        if(window.confirm('Are you sure? '+ fullName +' will be permanently deleted.')) {
+                            $.ajax({
+                                method: 'POST',
+                                data: {
+                                    user_id: userId,
+                                    f_name: fname,
+                                    l_name: lname
+                                },
+                                url: 'database/delete-user.php',
+                                dataType: 'json',
+                                success: function(data){
+                                    if(data.success){
+                                        if(window.confirm(data.message)){
+                                            location.reload();
+                                        }
+                                    } else window.alert(data.message);
+                                }
+                            })
+                        } else {
+                            console.log('cancelled');
+                        }
+                    }
+                });
+            }
+
+        }
+
+        var script = new script;
+        script.initialize();
+    </script>
 </body>
 
 </html>
